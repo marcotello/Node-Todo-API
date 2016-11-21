b429483a62431e29305acbcd30f78e2338c7c2ff
Marco Tello <muchaweb@gmail.com>
2016-11-21 14:10:47 -0600

    Add POST for todos

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-		id: 1,
-		description: 'Send the email',
-		completed: false
-	}, {
-		id: 2,
-		description: 'Feed the dogs',
-		completed: false
-	}, {
-		id: 3,
-		description: 'Check the poll',
-		completed: true
-	}];
+var todoNextId = 1;
+// var todos = [{
+// 		id: 1,
+// 		description: 'Send the email',
+// 		completed: false
+// 	}, {
+// 		id: 2,
+// 		description: 'Feed the dogs',
+// 		completed: false
+// 	}, {
+// 		id: 3,
+// 		description: 'Check the poll',
+// 		completed: true
+// 	}, {
+// 		id: 4,
+// 		description: 'Charge my phone',
+// 		completed: true
+// 	}];
+
+var todos = [];
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('TODO API Root');
@@ -58,6 +69,23 @@ app.get('/todos/:id', function (req, res) {
 
 	//res.json(todos[todoIndex]);
 	//res.send('ID = ' + req.params.id);
+});
+
+app.post('/todos', function (req, res) {
+	var body = req.body;
+	console.log('description: ' + body.description);
+
+	var todo = {};
+
+	todo.id = todoNextId;
+	todo.description = body.description;
+	todo.completed = body.completed;
+
+	todos.push(todo)
+
+	todoNextId++;
+
+	res.json(todo);
 });
 
 app.listen(PORT, function () {
