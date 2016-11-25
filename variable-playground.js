6fd63457a87ebec25b6cef94704c2887e8e6e64f
Marco Tello <muchaweb@gmail.com>
2016-11-25 14:05:05 -0600

    Adding the playground to the project

---
+++ variable-playground.js
@@ -0,0 +1,24 @@
+// var person = {
+// 	name: 'MArco',
+// 	age: 34
+// }
+
+// function updatePerson(obj){
+// 	obj = {
+// 		name: 'Marco',
+// 		age: 30
+// 	};
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15, 88];
+
+function addGrades (grades) {
+	grades.push(55);
+	debugger;
+}
+
+addGrades(grades);
+console.log(grades);
