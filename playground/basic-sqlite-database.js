819bc653dac909a8e388d8781157fdaf2de01b9a
Marco Tello <muchaweb@gmail.com>
2016-11-26 20:30:08 -0600

    Fetching data by different mechanisms

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -6,21 +6,76 @@ var sequelize = new Sequelize(undefined, undefined, undefined, {
 
 var Todo = sequelize.define('todo', {
 	description: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		// Adding validation
+		allowNull: false,
+		validate: {
+			len: [1, 250]
+		}
 	},
 	completed: {
-		type: Sequelize.BOOLEAN	
+		type: Sequelize.BOOLEAN,
+		// Adding validation
+		allowNull: false,
+		defaultValue: false
 	}
 });
 
-sequelize.sync({force:true}).then(function () {
+// Force the database re created from the ground
+// sequelize.sync({force:true}).then(function () {
+// 	console.log('Everything is synced');
+
+// 	Todo.create({
+// 		//description: 'Walk my dog',
+// 		completed: false
+// 	}).then(function (todo){
+// 		console.log('Finished');
+// 		console.log(todo);
+// 	});
+// });
+
+sequelize.sync(
+	//{force:true}
+	).then(function () {
 	console.log('Everything is synced');
 
-	Todo.create({
-		description: 'Walk my dog',
-		completed: false
-	}).then(function (todo){
-		console.log('Finished');
-		console.log(todo);
+	Todo.findById(3).then(function (todo) {
+		if(todo){
+			console.log(todo.toJSON());
+		}else{
+			console.log('Not data found');
+		}
+	}).catch( function (e) {
+		console.log(e);
 	});
+
+	// Todo.create({
+	// 	description: 'Make the test',
+	// 	//completed: false
+	// }).then(function (todo){
+	// 	//console.log('Finished');
+	// 	//console.log(todo);
+	// 	return Todo.create({
+	// 		description: 'Clean the office'
+	// 	});
+	// }).then( function () {
+	// 	//return Todo.findById(1);
+	// 	return Todo.findAll({
+	// 		where: {
+	// 			description: {
+	// 				$like: '%office%'
+	// 			}
+	// 		}
+	// 	});
+	// }).then( function (todos){
+	// 	if(todos){
+	// 		todos.forEach( function (todo){
+	// 			console.log(todo.toJSON());
+	// 		});
+	// 	}else{
+	// 		console.log('No todo finded');
+	// 	}
+	// }).catch( function (e) {
+	// 	console.log(e);
+	// });
 });
